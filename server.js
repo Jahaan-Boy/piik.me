4f733e3561e3c4f995e40c9c0b328b1fe9b5e609
Athar <myselfatharakram@gmail.com>
2025-12-04 21:01:48 +0530

    Fix bio link view tracking with graceful error handling

--- server.js
+++ server.js
@@ -478,7 +478,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get(['/home', '/analytics', '/profile', '/qr-generator'], (req, res) => {
+app.get(['/home', '/analytics', '/profile', '/qr-generator', '/bio-link'], (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
